d16810eb187f71c67614685e22f9ab2eca6f99da
Anna <ann.kazannik@gmail.com>
2024-12-12 18:25:19 -0500

    Setup eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello world
-      </header>
+      <header className="App-header">Hello world</header>
     </div>
   );
-}
+};
 
 export default App;

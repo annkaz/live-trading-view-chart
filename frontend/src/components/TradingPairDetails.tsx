b5947646a4ba413d450b7740de48bac3648cc0d2
Anna <ann.kazannik@gmail.com>
2024-12-18 12:55:12 -0700

    Fetch available symbols

--- frontend/src/components/TradingPairDetails.tsx
+++ frontend/src/components/TradingPairDetails.tsx
@@ -3,7 +3,7 @@ import EthereumIcon from "../assets/EthereumIcon";
 import { useWebSocket } from "../context/WebSocketProvider";
 import { fetchTradingPairInfo } from "../services/vestApi";
 
-const SYMBOL = "ETH-PERP";
+const DEFAULT_SYMBOL = "ETH-PERP";
 
 type PairData = {
   price: string;
@@ -14,13 +14,15 @@ type PairData = {
 
 const TradingPairDetails = () => {
   const [pairData, setPairData] = useState<PairData | null>(null);
+  const [availableSymbols, setAvailableSymbols] = useState<string[]>([]);
+  const [selectedSymbol, setSelectedSymbol] = useState(DEFAULT_SYMBOL);
 
   const { subscribe } = useWebSocket();
 
   useEffect(() => {
     const fetchInitialData = async () => {
       try {
-        const data = await fetchTradingPairInfo(SYMBOL);
+        const data = await fetchTradingPairInfo(selectedSymbol);
         setPairData(data);
       } catch (error) {
         console.error("Error fetching trading pair info:", error);
@@ -32,7 +34,14 @@ const TradingPairDetails = () => {
 
   useEffect(() => {
     const handleWebSocketUpdate = (data: any[]) => {
-      const ticker = data.find((item: any) => item.symbol === SYMBOL);
+      // Fetch data for available symbols dropdown
+      const symbols = data.map((item: any) => item.symbol);
+      // Make sure to populate only once
+      setAvailableSymbols((prev) =>
+        prev.length === 0 ? Array.from(new Set(symbols)) : prev
+      );
+
+      const ticker = data.find((item: any) => item.symbol === selectedSymbol);
       if (ticker) {
         setPairData((prevData) => ({
           ...prevData,
@@ -53,8 +62,8 @@ const TradingPairDetails = () => {
 
   // Update the document title whenever the price changes
   useEffect(() => {
-    document.title = `${pairData?.price} | ${SYMBOL} | Vest`;
-  }, [pairData?.price]);
+    document.title = `${pairData?.price} | ${selectedSymbol} | Vest`;
+  }, [pairData?.price, selectedSymbol]);
 
   if (!pairData) return null;
 
@@ -62,7 +71,7 @@ const TradingPairDetails = () => {
     <div className="flex w-full items-center justify-between p-4 border-b border-gray-700">
       <div className="flex items-center space-x-2">
         <EthereumIcon />
-        <span className="font-semibold">ETH/USDC</span>
+        <span className="font-semibold">{selectedSymbol}</span>
       </div>
       <div>
         <div className="text-sm text-gray-400">PRICE</div>
